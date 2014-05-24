059cdabe0d7c9f602a71394e85910483d11bc9ca
deoxxa <deoxxa@fknsrs.biz>
2014-05-24 21:07:02 +1000

    make chat work (and more!)

--- test/index.js
+++ test/index.js
@@ -1,7 +1,27 @@
-var ApplicationStream = require("../lib/application-stream");
+var assert = require("chai").assert;
 
-describe("the application stream", function() {
-  it("shouldn't suck", function(done) {
-    return done(Error("it sucks"));
+var Application = require("../lib/application");
+
+describe("the application", function() {
+  it("should pass chat messages from one user to another", function(done) {
+    var application = new Application(),
+        user1 = application.createUser(),
+        user2 = application.createUser();
+
+    user2.on("data", function(message) {
+      assert.equal(message.type, "chat");
+      assert.equal(message.data.target, "user2");
+      assert.equal(message.data.message, "hello");
+
+      return done();
+    });
+
+    user1.write({
+      type: "chat",
+      data: {
+        target: "user2",
+        message: "hello",
+      },
+    });
   });
 });
